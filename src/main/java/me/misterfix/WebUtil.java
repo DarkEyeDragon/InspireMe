8d993b72162ef1ffe73920575d82ab75318f85a1
Trigary <trigary0@gmail.com>
2018-10-02 23:37:20 +0200

    general cleanup

--- src/main/java/me/misterfix/WebUtil.java
+++ src/main/java/me/misterfix/WebUtil.java
@@ -1,17 +1,21 @@
 package me.misterfix;
 
-import java.io.BufferedReader;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.TextChannel;
+import net.dv8tion.jda.core.utils.IOUtil;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import net.dv8tion.jda.core.entities.Member;
-import net.dv8tion.jda.core.entities.TextChannel;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class WebUtil {
+
+    private static final Pattern CARRIAGE_RETURN_NEWLINE_PATTERN = Pattern.compile("\r\n", Pattern.LITERAL);
 
     public static String getWebPage(String url) throws IOException {
         URL searchURL = new URL(url);
@@ -20,7 +24,6 @@ public class WebUtil {
     }
 
     public static String getWebPage(HttpURLConnection conn) throws IOException {
-        StringBuilder sb = new StringBuilder();
         conn.setRequestProperty("User-Agent", "InspireMe/1.3");
 
         int response = conn.getResponseCode();
@@ -28,21 +31,20 @@ public class WebUtil {
             System.out.println("DEBUG: Response code: " + response);
         }
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line;
-        while ((line = in.readLine()) != null) {
-            sb.append(line).append("\n");
-        }
-        in.close();
+        return CARRIAGE_RETURN_NEWLINE_PATTERN.matcher(new String(IOUtil.readFully(conn.getInputStream())))
+                .replaceAll(Matcher.quoteReplacement("\n"))
+                .replace('\r', '\n');
+    }
 
-        return sb.toString();
+    public static String getQuoteAPi(Member member, TextChannel channel, String url) {
+        return getApi(member, channel, "https://api.darkeyedragon.me/quotes/" + url);
     }
 
     public static String getApi(Member member, TextChannel channel, String url) {
         String json;
         try {
             json = WebUtil.getWebPage(url);
-        } catch (IOException e) {
+        } catch (IOException ignored) {
             MessageFactory.createStandardMessage(member, "#BlameDarkEyeDragon")
                 .setDescription("Could not contact DarkEyeDragon's API.")
                 .queue(channel);
